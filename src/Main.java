f5e517a5999e6eccacc5669b0e16e172e2c08d89
Megadubcev2 <aestarodubtsev@edu.hse.ru>
2022-12-05 22:31:06 +0300

    не работает пока

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import Controller.Game;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Игра временно не работает, просьба проверить чуть попозже)");
         var game = new Game();
         game.play();
     }
